e6b7cd2072503fecdac0a22b27eabfa7fa07e77b
unknown <michael.chian9@gmail.com>
2023-08-05 23:02:05 -0700

    started readme

---
+++ README.md
@@ -0,0 +1,9 @@
+# Basketball Shooting Coach
+
+This project attempts to detect the user's basketball shooting form using [YOLO](https://github.com/ultralytics/ultralytics) object detection and [OpenPose](https://github.com/CMU-Perceptual-Computing-Lab/openpose).
+
+
+
+## Identifying a Basketball
+
+<img src="https://github.com/MichaelChian452/bball-shooting-coach/assets/43621839/c0097319-9c26-419c-8976-19bd54db4f0f" alt="Indy and Basketball" width="250"/>
--- predict.py
+++ predict.py
@@ -6,9 +6,44 @@ import cv2
 
 PRACC_DIR = os.path.join('.', 'pracc')
 
-ball1_path = os.path.join(PRACC_DIR, 'ball2_40.jpg')
+ball1_path = os.path.join(PRACC_DIR, 'cropped.jpg')
+
+# Image Resize method, based on dimensions or aspect ratio
+def image_resize(image, width = None, height = None, inter = cv2.INTER_AREA):
+    # initialize the dimensions of the image to be resized and
+    # grab the image size
+    dim = None
+    (h, w) = image.shape[:2]
+
+    # if both the width and height are None, then return the
+    # original image
+    if width is None and height is None:
+        return image
+
+    # check to see if the width is None
+    if width is None:
+        # calculate the ratio of the height and construct the
+        # dimensions
+        r = height / float(h)
+        dim = (int(w * r), height)
+
+    # otherwise, the height is None
+    else:
+        # calculate the ratio of the width and construct the
+        # dimensions
+        r = width / float(w)
+        dim = (width, int(h * r))
+
+    # resize the image
+    resized = cv2.resize(image, dim, interpolation = inter)
+
+    # return the resized image
+    return resized
 
 frame = cv2.imread(ball1_path)
+
+frame = image_resize(frame, height=1000)
+
 
 # Model with 25 epochs
 # model_path = os.path.join('.', 'runs', 'detect', 'train9', 'weights', 'best.pt')
@@ -18,20 +53,21 @@ model_path = os.path.join('.', 'best.pt')
 
 # -- Testing on image --
 
-# model = YOLO(model_path)
-# results = model(frame)[0]
-# threshold = 0.5
+model = YOLO(model_path)
+results = model(frame)[0]
+threshold = 0.5
 
-# for result in results.boxes.data.tolist():
-#     x1, y1, x2, y2, score, class_id = result
+for result in results.boxes.data.tolist():
+    x1, y1, x2, y2, score, class_id = result
 
-#     if score > threshold:
-#         cv2.rectangle(frame, (int(x1), int(y1)), (int(x2), int(y2)), (0, 255, 0), 4)
-#         cv2.putText(frame, results.names[int(class_id)].upper(), (int(x1), int(y1 - 10)),
-#                     cv2.FONT_HERSHEY_SIMPLEX, 1.3, (0, 255, 0), 3, cv2.LINE_AA)
+    if score > threshold:
+        cv2.rectangle(frame, (int(x1), int(y1)), (int(x2), int(y2)), (0, 255, 0), 4)
+        cv2.putText(frame, results.names[int(class_id)].upper(), (int(x1), int(y1 - 10)),
+                    cv2.FONT_HERSHEY_SIMPLEX, 1.3, (0, 255, 0), 3, cv2.LINE_AA)
 
-# cv2.imshow('cat', frame)
-# cv2.waitKey(0)
+cv2.imshow('cat', frame)
+cv2.imwrite('result.jpg', frame)
+cv2.waitKey(0)
 
 # -- Testing on video --
 
